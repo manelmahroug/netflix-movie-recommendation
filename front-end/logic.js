c3b8547bc2e0829a17a23c4ce1b0cc496d3a40d8
Kevin McCurdy <khmccurdy@aol.com>
2018-07-18 21:14:40 -0700

    Submit now responds to ratings

--- front-end/logic.js
+++ front-end/logic.js
@@ -1,5 +1,6 @@
 // replace movies array with real list of titles from csv
 var movies = ["Reservoir Dogs","Lord of the Rings","The Room","Jurassic Park","Jaws"];
+// var movieIndices = [];
 const numInputs = 5;
 
 d3.text("../movie_titles.csv",(e,r)=>{
@@ -7,6 +8,7 @@ d3.text("../movie_titles.csv",(e,r)=>{
     var data = d3.csvParseRows(r)
     console.log(data[0]);
     movies = data.map(d=>d[2])
+    // data.forEach(d=>{movieIndices[d[2]] = +d[0]});
 
     for (var i=1; i<=numInputs; i++){
         autocomplete(document.getElementById(`movieInput${i}`), movies);
@@ -38,19 +40,22 @@ d3.select("#moviesSubmit").on("click",function(event){
     d3.event.preventDefault();
 
     titles = [];
+    ratings = [];
     for (var i=1; i<=numInputs; i++){
         titles.push(d3.select(`#movieInput${i}`).node().value.trim());
+        ratings.push(+d3.select(`#rating${i}`).node().value);
     }
     console.log(titles);
-    dummyResponse($responseText, titles);
+    dummyResponse($responseText, titles, ratings);
 })
 
-function dummyResponse($div, list){
+function dummyResponse($div, tList, rList){
     arr = [];
-    list.forEach(d=>{arr.push(...d.split(" "))});
+    tList.forEach(d=>{arr.push(...d.split(" "))});
+    // console.log(movieIndices[list[0]]);
     arr = arr.sort(d=>(Math.random()-0.5));
     output = "";
     arr.forEach(d=>{output += d + " "})
-    console.log(arr)
-    $div.text(`Based on your input, we think you'll like "${output}".`)
+    // console.log(arr)
+    $div.text(`Based on your input, we think you'll like "${output}". Siskel and Ebert gave it ${d3.mean(rList)} thumbs up.`)
 }
